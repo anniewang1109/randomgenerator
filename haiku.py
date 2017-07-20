5691776d9ef32cdf7c8574dbb85fca5b0c1ad5c5
wangan9 <wangan@bxscience.edu>
2017-07-20 15:10:34 -0400

    Add files via upload

---
+++ haiku.py
@@ -0,0 +1,14 @@
+import random
+
+
+random_num = random.randint(0,16)
+random_num2 = random.randint(0,10)
+random_num3 = random.randint(0,16)
+
+
+five = ["the cold is coming","invincible you","leaves are falling down","girls who code is fun","coding is useful","how are you doing","i like to play sports","i like to draw things","the subway is slow","scratch is annoying","i am so hungry","the bank stores money","code is colorful","you are beautiful","lets play mafia", "do you want to hang","ella is the god"]
+seven = ["skyscrapers are very tall","follow your dreams everyone","what are you doing right now","school is starting very soon","we respect one another","i like to drink snapple tea","the food is very good here","coding can be fustrating","we are not afraid to fail","the bank of america","the potato game is cool"]
+
+print (five[random_num])
+print (seven[random_num2])
+print (five[random_num3])
